f5e264e61d6a18a6805d0f0caf24c0354fe65a31
熊望 <bearsee@163.com>
2020-06-18 16:49:20 +0800

    1

--- pages/login/pwdLogin.js
+++ pages/login/pwdLogin.js
@@ -16,17 +16,19 @@ Page({
     this.setData({ password });
   },
   login() {
-    const { mobileNumber, password } = this.data;
-    wx.$http.post('init/login', { mobileNumber, password }, true).then(
-      res => {
-        app.finishLogin(res);
-        wx.setStorageSync('mobileNumber', mobileNumber);
-        wx.setStorageSync('password', password);
-      },
-      err => {
-        // app.showModal({ content: err.message || '登录失败', hiddenCancel: true, confirmText: '好的' });
-      }
-    );
+    setTimeout(() => {
+      const { mobileNumber, password } = this.data;
+      wx.$http.post('init/login', { mobileNumber, password }, true).then(
+        res => {
+          app.finishLogin(res);
+          wx.setStorageSync('mobileNumber', mobileNumber);
+          wx.setStorageSync('password', password);
+        },
+        err => {
+          // app.showModal({ content: err.message || '登录失败', hiddenCancel: true, confirmText: '好的' });
+        }
+      );
+    }, 200);
   },
   gotoRegister() {
     wx.navigateTo({ url: '/pages/register/register' });
